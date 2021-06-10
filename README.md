a8a2e1a38fdc6c48006901daf4c3fe1067a30658
Sergio Barria <sergio.barria@icloud.com>
2021-06-10 17:17:33 -0500

    updated url on script file

--- README.md
+++ README.md
@@ -31,8 +31,8 @@ Users should be able to:
 
 ### Links
 
-- Solution URL: [Add solution URL here](https://your-solution-url.com)
-- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+- Solution URL: [URL here](https://easybank-sergiobarria.netlify.app/)
+- Live Site URL: [URL here](https://easybank-sergiobarria.netlify.app/)
 
 ## My process
 
--- js/script.js
+++ js/script.js
@@ -3,11 +3,19 @@ const toggle = document.getElementById("hamburger");
 const modal = document.getElementById("modal");
 
 toggle.addEventListener("click", () => {
-  if (toggle.src === "http://127.0.0.1:5500/images/icon-close.svg") {
-    document["nav-img"].src = "http://127.0.0.1:5500/images/icon-hamburger.svg";
+  if (
+    toggle.src ===
+    "https://easybank-sergiobarria.netlify.app/images/icon-close.svg"
+  ) {
+    document["nav-img"].src =
+      "https://easybank-sergiobarria.netlify.app/images/icon-hamburger.svg";
     modal.classList.toggle("show-modal");
-  } else if (toggle.src === "http://127.0.0.1:5500/images/icon-hamburger.svg") {
-    document["nav-img"].src = "http://127.0.0.1:5500/images/icon-close.svg";
+  } else if (
+    toggle.src ===
+    "https://easybank-sergiobarria.netlify.app/images/icon-hamburger.svg"
+  ) {
+    document["nav-img"].src =
+      "https://easybank-sergiobarria.netlify.app/images/icon-close.svg";
     modal.classList.toggle("show-modal");
   }
 });
